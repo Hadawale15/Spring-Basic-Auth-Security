b5dbff0bc3def51d940a69f286e4b8d55641c7d8
Nandiwaleamol <nandivaleamol@gmail.com>
2025-08-01 18:38:15 +0530

    basic auth

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -11,11 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,7 +30,6 @@ public class SecurityConfig {
         daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return  daoAuthenticationProvider;
     }
-
 
     @Bean
     public SecurityFilterChain  securityFilterChain(HttpSecurity httpSecurity) throws Exception{
